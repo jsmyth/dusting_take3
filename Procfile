1156eded75a196a926b265ebbc325980a0eaa155
Jim <jwsmyth@comcast.net>
2020-11-20 21:33:29 +0000

    "Initial commit. Generated by cookiecutter-flask"

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: flask db upgrade
+web: gunicorn dusting_take3.app:create_app\(\) -b 0.0.0.0:$PORT -w 3
---
+++ autoapp.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Create an application instance."""
+from dusting_take3.app import create_app
+
+app = create_app()
